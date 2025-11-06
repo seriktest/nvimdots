a67cdec561d60fa99d82e791e55870ea047b4c0b
Бейманов Сергей <serik@MacBook-Pro-Bejmanov.local>
2025-11-06 10:05:27 +0300

    123

--- lua/modules/configs/completion/copilot.lua
+++ lua/modules/configs/completion/copilot.lua
@@ -11,11 +11,7 @@ return function()
 			},
 			suggestion = {
 				-- if true, it can interfere with completions in copilot-cmp
-<<<<<<< HEAD
-				enabled = false,
-=======
 				enabled = true,
->>>>>>> ff29005 (local)
 			},
 			filetypes = {
 				["dap-repl"] = false,
